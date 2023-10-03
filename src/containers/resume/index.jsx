1f52c4c65b1c98d6451609c61b0915e8325dbb74
nasrin jafari <nasrin.jafari770@gmail.com>
2023-10-04 01:13:51 +0330

    design resume page

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { BsInfoCircleFill } from "react-icons/bs";
-import { MdLocalLibrary } from "react-icons/md"
+import { MdLocalLibrary } from "react-icons/md";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
